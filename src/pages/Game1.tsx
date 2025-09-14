c6c749867e5a9deda94d257cf5ca5789ea0c86ac
jaehyun0620 <zzz6451@naver.com>
2025-09-14 22:47:19 +0900

    ✨ feat: 없는 게 없는 무도 게임 기본 레이아웃 틀 작업 진행

--- src/pages/Game1.tsx
+++ src/pages/Game1.tsx
@@ -1,7 +1,20 @@
 export default function Game1() {
   return (
     <>
-      <h1>게임1 예시 페이지 입니다.</h1>
+      <div className="bg-yellow-200 min-w-[360px] min-h-screen">
+        <h1 className="text-[36px] text-center">없는 게 없는 무도 게임</h1>
+        <p className="text-[16px] text-center">선택한 캐릭터와 관련된 짤을 보고 정답을 맞혀보세요!</p>
+        <div className="flex justify-between pl-2 pr-2">
+          <div className="w-[48px] h-[93px] bg-blue-500"></div>
+          <div className="w-[48px] h-[93px] bg-blue-500"></div>
+          <div className="w-[48px] h-[93px] bg-blue-500"></div>
+          <div className="w-[48px] h-[93px] bg-blue-500"></div>
+          <div className="w-[48px] h-[93px] bg-blue-500"></div>
+          <div className="w-[48px] h-[93px] bg-blue-500"></div>
+          <div className="w-[48px] h-[93px] bg-blue-500"></div>
+        </div>
+        <button>퀴즈 출바알</button>
+      </div>
     </>
   );
 }
