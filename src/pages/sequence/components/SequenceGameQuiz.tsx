7d6a40e02133309deeddb1b3a76735ee60608501
onewayay <onewayay@naver.com>
2025-10-13 20:28:48 +0900

    ✨ 짤 순서 맞추기 게임 개발  - 답 입력 input 제출 후 clear 및 focus 되도록  - round 부분 상태 처리  - 마지막 문제 풀고나서 결과보기 페이지 개발  - 답 중복될 경우 어떻게 할지 처리  - 다음 라운디 이미지 미리 불러오기  - 컴포넌트 분리  - 반응형 1차 완료

---
+++ src/pages/sequence/components/SequenceGameQuiz.tsx
@@ -0,0 +1,53 @@
+import type { Images } from '../../../libs/api';
+
+interface SequenceGameQuizType {
+  currentPhotos: Images[];
+  isSolving: boolean;
+}
+
+export default function SequenceGameQuiz({ currentPhotos, isSolving }: SequenceGameQuizType) {
+  const quizList = currentPhotos.map((item, i) => {
+    // 사진 3장 li로 삽입
+    if (isSolving === true) {
+      // 문제 푸는 중
+      return (
+        <li key={i} className="flex-1">
+          <figure className="flex flex-col justify-between items-center h-full">
+            <div className="relative">
+              <img src={item.imgPath} alt={`${i + 1}번 문제 사진`} className="max-h-[150px] md:max-h-none" />
+              <span className="absolute top-0 left-0 -translate-x-1/2 -translate-y-1/2 w-5 h-5 text-center content-center bg-dark-red font-medium text-white text-xs md:w-6 md:h-6 md:text-sm lg:w-7 lg:h-7 lg:text-base">
+                {i + 1}
+              </span>
+            </div>
+            <figcaption className="font-medium text-center lg:base"></figcaption>
+          </figure>
+        </li>
+      );
+    } else {
+      // 답변 제출 상태 (답(회차정보) 표출)
+      return (
+        <li key={i} className="flex-1">
+          <figure className="flex flex-col justify-between items-center gap-2 h-full md:gap-5 lg:gap-7">
+            <div className="relative">
+              <img src={item.imgPath} alt={`${i + 1}번 문제 사진`} className="max-h-[150px] md:max-h-none" />
+              <span className="absolute top-0 left-0 -translate-x-1/2 -translate-y-1/2 w-5 h-5 text-center content-center bg-dark-red font-medium text-white text-xs md:w-6 md:h-6 md:text-sm lg:w-7 lg:h-7 lg:text-base">
+                {i + 1}
+              </span>
+            </div>
+            <figcaption className="font-medium text-center leading-[1.2] md:leading-normal lg:base">
+              <p className="text-xs md:text-sm lg:text-base">
+                {item.episodeNum}회 ({item.episodeDate})
+              </p>
+              <strong className="font-medium text-xs md:text-sm lg:text-base">{item.episodeName}</strong>
+            </figcaption>
+          </figure>
+        </li>
+      );
+    }
+  });
+  return (
+    <ul className="flex flex-col items-stretch justify-between gap-[20px] md:flex-row md:gap-[30px] mt-[40px] md:mt-[60px] lg:gap-[40px] lg:mt-20">
+      {quizList}
+    </ul>
+  );
+}
---
+++ src/pages/sequence/components/SequenceHomeText.tsx
@@ -0,0 +1,12 @@
+export default function SequenceHomeText() {
+  return (
+    <div className="text-center">
+      <h2 className="font-bold text-[2.125rem] md:text-[3.125rem] lg:text-[3.75rem]">무도 짤 순서 맞추기 게임</h2>
+      <p className="leading-[1.6] mt-4 text-base md:text-lg lg:mt-6 lg:text-xl">
+        화면의 보여지는 짤들을 회차 순으로 정렬해보세요!
+        <br />
+        (오래된 회차 -&gt; 최신 회차)
+      </p>
+    </div>
+  );
+}
